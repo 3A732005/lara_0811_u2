0b56a99d84ced00d386b94b069819197111f306f
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-20 23:10:50 +0800

    HomeController相關之操作

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return 'welcome';
-});
+});*/
+
+Route::get('/',['as'=>'home.index','user'=>'HomeController@index']);
 
 Route::get('r1', function () {
     return redirect('r2');
